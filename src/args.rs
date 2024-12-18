410d1e0969ebf257bc4d1e48cabfa59ce1d40670
Christoph Wulf <christoph.wulf@talentformation.com>
2024-12-18 22:10:40 +0100

    Trigger inter

--- src/args.rs
+++ src/args.rs
@@ -156,9 +156,11 @@ impl Args {
         I: IntoIterator<Item = T>,
         T: Into<std::ffi::OsString> + Clone,
     {
-        let args = Args::try_parse_from(itr)?;
-        args.validate().await?;
-        Ok(args)
+
+
+            let args = Args::try_parse_from(itr)?;
+                    args.validate().await?;
+                        Ok(args)
     }
 
     async fn validate(&self) -> Result<()> {
