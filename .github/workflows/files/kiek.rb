8bb5bc25126b1c9a40b00b45d6a5ad6822c19a20
Christoph Wulf <christoph.wulf@talentformation.com>
2024-12-29 14:56:03 +0100

    Create Homebrew tap update PR in CI

---
+++ .github/workflows/files/kiek.rb
@@ -0,0 +1,24 @@
+class kiek < Formula
+  version '%VERSION%'
+  sha256_mac '%SHA256_MAC%'
+  sha256_linux '%SHA256_LINUX%'
+  desc "kiek helps you to look into Kafka topics."
+  homepage "https://github.com/wulf-data-engineering/kiek"
+
+  if OS.mac?
+      url "https://github.com/wulf-data-engineering/kiek/releases/download/v#{version}/kiek-aarch64-apple-darwin.tar.gz"
+      sha256 "#{sha256_mac}"
+  elsif OS.linux?
+      url "https://github.com/wulf-data-engineering/kiek/releases/download/v#{version}/kiek-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "#{sha256_linux}"
+  end
+
+  conflicts_with "kiek"
+
+  def install
+    bin.install "kiek"
+    zsh_completion.install "completions/_kiek"
+    bash_completion.install "completions/kiek.bash"
+    fish_completion.install "completions/kiek.fish"
+  end
+end
